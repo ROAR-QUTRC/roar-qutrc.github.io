f7d293b1f17ec330e49d5fb9bea1cf39cd75ba4b
huon <huonswales@gmail.com>
2026-07-03 12:16:56 +1000

    first commit

---
+++ devenv.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.NO_MKDOCS_2_WARNING = 1;
+
+  packages = with pkgs; [ 
+      doxygen
+      python313Packages.mkdocs-material
+      python313Packages.mkdocs-material-extensions
+   ] ++ python313Packages.mkdocs-material.optional-dependencies.imaging;
+
+  languages = {
+    python.enable = true;
+    javascript = {
+      enable = true;
+      package = pkgs.nodejs_22;
+      npm = {
+        enable = true;
+        install.enable = true;
+      };
+    };
+  };
+
+  scripts = {
+    dev.exec = "mkdocs serve";
+  };
+}
